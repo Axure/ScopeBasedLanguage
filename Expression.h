482b9d9fcc74dd919103a37b4f4e1ecc95849c7b
Axure <axurez@vip.qq.com>
2015-05-05 05:32:11 +0800

    New class for expression.

---
+++ Expression.h
@@ -0,0 +1,14 @@
+//
+// Created by 郑虎 on 15 年 五月. 4..
+//
+
+#ifndef SCOPEBASEDLANGUAGE_EXPRESSION_H
+#define SCOPEBASEDLANGUAGE_EXPRESSION_H
+
+
+class Expression {
+
+};
+
+
+#endif //SCOPEBASEDLANGUAGE_EXPRESSION_H
